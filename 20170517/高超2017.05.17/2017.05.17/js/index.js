bd498973a2bb272551b731e3ee9b8711cc2dbd4c
mingzhezc123 <1095889023@qq.com>
2017-06-02 10:54:09 +0800

    add 20170501/16/17

---
+++ 20170517/高超2017.05.17/2017.05.17/js/index.js
@@ -0,0 +1,75 @@
+// for(var i=1;i<=1000; i++){
+//      if(i%5==0&&i%6==0){
+//         console.log(i);
+//      }
+// }
+
+// var n=parseInt(prompt("请输入一个数字。"));
+// var jieguo=0;
+// for( var i=1; i<=n;i++){
+//         jieguo+=i;
+// }
+// alert(jieguo);
+
+// var n=parseInt(prompt("请输入一个数字。"))
+// var jieguo=0;
+// for(var i=2; i<=n;i++){
+//        jieguo=jieguo+(i+1)/i;
+// }alert(jieguo);
+
+// var n=parseInt(prompt("请输入一个数字。"))
+// for(var i=1; i<=n; i++){
+//     if (n%i==0){
+//         console.log(i);
+//     }
+// }
+
+// for( var i=1; i<=60; i++){
+//     if(i%7!=0&&i%10!=7){
+//     console.log(i)
+// }
+// }
+// for(var i=100;i<=1000;i++){
+//     var gewei=i%10;
+//     var shiwei=parseInt(i/10%10);
+//     var baiwei=parseInt(i/100);
+//     if(Math.pow(gewei,3)+Math.pow(shiwei,3)+Math.pow(baiwei,3)==i){
+//         console.log(i);
+//     }
+// }
+// 第七
+// for(var i=1; i<=10000; i++){
+//     var  sum=0;
+//      for(var j=1; j<i; j++){
+//         if(i%j==0){
+//             sum=sum+j;
+//         }
+//      }
+//      if(sum==i){
+//         console.log(i);
+//      }
+// }
+
+
+// var n=parseInt(prompt("请输入一个数字。"));
+//  var cound=0;
+//      for(var j=1;j<=n;j++){
+//         if(n%j==0){
+//             cound++;
+//      }
+// } 
+//     if(cound==2){
+//         alert(n+"是质数");
+//      }else{
+//         alert(n+"不是质数");
+//      }
+// for(var i=2; i<=10000;i++){
+//     cound=0;
+//     for(var j=1; j<=i;j++){
+//         if(i%j==0){
+//              cound++;
+//         }
+//     }if(cound==2){
+//         console.log(i);
+//     }
+// }
